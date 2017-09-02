4e6123a6064df441fe235a88e4241dcd24238388
JasonMcKenney <jasonpmckenney@gmail.com>
2017-09-02 11:38:11 -0400

    Added basic routes and controllers to the Admin area

--- ASPBlog/App_Start/RouteConfig.cs
+++ ASPBlog/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using ASPBlog.Controllers;
 
 namespace ASPBlog
 {
@@ -11,13 +12,12 @@ namespace ASPBlog
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] { typeof(PostsController).Namespace };
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Login", "login", new { Controller = "Auto", action = "Login" }, namespaces);
+
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
         }
     }
 }
---
+++ ASPBlog/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ASPBlog.Areas.Admin.Controllers
+{
+    public class PostsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Posts!");
+        }
+    }
+}
