21a1f73fb0482ad394f964a2e01a433695c41693
JasonMcKenney <jasonpmckenney@gmail.com>
2017-09-17 20:00:30 -0400

    Admin layout added

--- ASPBlog/Areas/Admin/Controllers/PostsController.cs
+++ ASPBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASPBlog.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,7 @@ using System.Web.Mvc;
 namespace ASPBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTab("posts")]
     public class PostsController : Controller
     {
         public ActionResult Index()
--- ASPBlog/Areas/Admin/Controllers/UsersController.cs
+++ ASPBlog/Areas/Admin/Controllers/UsersController.cs
@@ -1,13 +1,15 @@
-﻿using System.Web.Mvc;
+﻿using ASPBlog.Infrastructure;
+using System.Web.Mvc;
 
 namespace ASPBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTab("users")]
     public class UsersController : Controller
     {
         public ActionResult Index()
         {
-            return Content("Users!");
+            return View();
         }
     }
 }
